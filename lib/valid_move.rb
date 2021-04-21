196f778ee66d2ac29e0de483c56201ffa9d53553
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-04-21 00:02:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,8 @@
-request "pry"
 # code your #valid_move? method here
 def valid_move
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   board[index]=="X" || board[index]=="O"
-  binding.pry
 end
