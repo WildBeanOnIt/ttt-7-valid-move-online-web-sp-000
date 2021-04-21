05a88c54d5babc10c071c33f7455f4f437d7f0b7
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-04-21 00:06:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
-def valid_move()
-  
+def valid_move?
+  display_board
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
